e4be07b2d8c7414db0ccd6155bacd85dfa75a9a8
Luciano-Lima-LL <limasantos678@gmail.com>
2024-08-17 15:28:54 -0300

    atualizei

---
+++ src/models/product.js
@@ -0,0 +1,19 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Category = sequelize.define('Category', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  slug: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  use_in_menu: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
+
+module.exports = Category;
